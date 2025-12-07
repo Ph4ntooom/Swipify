096d1d7d5bf28b25f339424e1d217a705c52a7fd
Aayan Hassan <ahi.aayanhassan@gmail.com>
2025-12-08 02:09:38 +0500

    Add swipe-based music recommendation logic
    Implemented endpoints for '/swipe_right' and '/swipe_left' to update user preference vector and generate new recommendations using cosine similarity on track features. Integrated sklearn and numpy for similarity calculations, and fixed Flask import. The server now returns updated recommendations based on user interactions.

--- Swipify/runserver.py
+++ Swipify/runserver.py
@@ -1,6 +1,9 @@
 from os import environ
+from sklearn.metrics.pairwise import cosine_similarity
+from Flask import flask, render_template, request
 from Swipify import app
 import pandas as pd
+import numpy as np
 import os
 from dotenv import load_dotenv
 import spotipy
@@ -24,6 +27,22 @@ df = pd.read_csv(csv_file)
 
 initial_recommendation = df.sample(5)
 
+features = ["danceability", "energy", "loudness", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo"]
+
+
+def get_recommendations(user_vector, last_track_id, num_recommendations=5):
+    df_filtered = df[df['track_id'] != last_track_id].copy()
+
+    song_vectors = df_filtered[features].values
+    user_vector_2d = user_vector.reshape(1, -1)
+
+    scores = cosine_similarity(user_vector_2d, song_vectors)[0]
+    
+    df_filtered['similarity'] = scores
+    recommendations = df_filtered.sort_values(by='similarity', ascending=False).head(num_recommendations)
+
+    recommended_tracks_info = [get_track_info(tid) for tid in recommendations['track_id']]
+    return recommended_tracks_info
 
 def get_track_info(track_id):
     track = sp.track(track_id)
@@ -51,19 +70,46 @@ def get_track_info(track_id):
     }
 
 tracks_info = [get_track_info(tid) for tid in initial_recommendation["track_id"]]
-print(tracks_info)
 
-user_vector
+
+
+# print(tracks_info)
+
 
 # Gotta Embed The SONG    
 #<iframe src="https://open.spotify.com/embed/track/{{ track_id }}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 
+user_vector = None
 
-#if __name__ == '__main__':
-#    HOST = environ.get('SERVER_HOST', 'localhost')
-#   try:
-#       PORT = int(environ.get('SERVER_PORT', '5555'))
-#    except ValueError:
-#        PORT = 5555
-#    app.run(HOST, PORT)
+@app.route('/swipe_right', methods=['GET', 'POST'])
+def swipe_right():
+    global user_vector
+    if request.method == 'POST':
+        track_id = request.form['track_id']
+        song_vector = df[df['track_id'] == track_id][features].values[0]
 
+        if user_vector is None:
+            user_vector = song_vector
+        else:
+            user_vector = ((user_vector + song_vector) / 2)
+
+        recommendations = get_recommendations(user_vector, track_id)
+        return render_template('index.html', tracks=recommendations)
+    return render_template('index.html', tracks=tracks_info)
+
+@app.route('/swipe_left', methods=['GET', 'POST'])
+def swipe_left():
+    if request.method == 'POST':
+        track_id = request.form['track_id']
+        recommendations = get_recommendations(user_vector, track_id)
+        return render_template('index.html', tracks=recommendations)
+    return render_template('index.html', tracks=tracks_info)
+
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '3000'))
+    except ValueError:
+         PORT = 3000
+    app.run(HOST, PORT)
