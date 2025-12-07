5f456ce79c8d8679e15efbd147917b77e4fa1faf
Ahad <abdulahad85101@gmail.com>
2025-12-08 01:09:05 +0500

    GET Track_info

--- Swipify/Swipify/spotify_data.py
+++ Swipify/Swipify/spotify_data.py
@@ -3,24 +3,21 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from sklearn.preprocessing import MinMaxScaler
 
-#Dependencies:
-#pip install flask spotipy python-dotenv scikit-learn numpy pandas
-
-# Load .env file
 load_dotenv()
 
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
 client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
 
-# Connect to Spotify
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(
-    client_id=client_id,
-    client_secret=client_secret
-))
+sp = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+)
 
-print(client_id)
-
+print("Client ID Loaded:", client_id)
 
 csv_file = "Songs Dataset.csv"
 df = pd.read_csv(csv_file)
@@ -28,9 +25,39 @@ df = pd.read_csv(csv_file)
 print("Columns in the dataset:")
 print(df.columns.tolist())
 
-print("\nFirst 5 rows of the dataset:")
-print(df.head())
+if "Unnamed: 0" in df.columns:
+    df = df.drop(columns=["Unnamed: 0"])
 
-#Make sure ur in DIR\Swipify\Swipify
-#run python Swipify\\spotify_data.py
+df = df.dropna()
+
+print("\nNull Records After Cleanup:")
+print(df.isnull().sum())
+
+df["explicit"] = df["explicit"].map({
+    "TRUE": 1, "True": 1, True: 1,
+    "FALSE": 0, "False": 0, False: 0
+}).astype(int)
+
+features = [
+    "popularity", "duration_ms", "explicit", "danceability", "energy",
+    "key", "loudness", "mode", "speechiness", "acousticness",
+    "instrumentalness", "liveness", "valence", "tempo", "time_signature"
+]
+
+for col in features:
+    df[col] = pd.to_numeric(df[col], errors="coerce")
+
+scaler = MinMaxScaler()
+df_scaled = scaler.fit_transform(df[features])
+
+df_norm = df.copy()
+for i, col in enumerate(features):
+    df_norm[col + "_norm"] = df_scaled[:, i]
+
+print("\nNormalized Feature Sample:")
+print(df_norm[[f + "_norm" for f in features]].head())
+
+df_norm.to_csv("Songs_Dataset_Normalized.csv", index=False)
+print("\nSaved normalized dataset!")
+print(df_norm.head())
 
--- Swipify/runserver.py
+++ Swipify/runserver.py
@@ -1,14 +1,69 @@
-"""
-This script runs the Swipify application using a development server.
-"""
-
 from os import environ
 from Swipify import app
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+dotenv_path = os.path.join(os.path.dirname(__file__), "Swipify", ".env")
+load_dotenv(dotenv_path)
+
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+sp = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials(
+        client_id=client_id,
+        client_secret=client_secret
+    )
+)
+
+csv_file = "Songs_Dataset_Normalized.csv"
+df = pd.read_csv(csv_file)
+
+initial_recommendation = df.sample(5)
+
+
+def get_track_info(track_id):
+    track = sp.track(track_id)
+
+    # Track name
+    track_name = track["name"]
+
+    # Artist(s) name(s) and first artist URL
+    artists = [a["name"] for a in track["artists"]]
+    artist_url = track["artists"][0]["external_urls"]["spotify"]
+
+    # Album image (biggest)
+    album_image_url = track["album"]["images"][0]["url"]
+
+    # Track Spotify URL
+    track_url = track["external_urls"]["spotify"]
+
+    return {
+        "track_id": track_id,
+        "track_name": track_name,
+        "artists": ", ".join(artists),
+        "artist_url": artist_url,
+        "album_image_url": album_image_url,
+        "track_url": track_url
+    }
+
+tracks_info = [get_track_info(tid) for tid in initial_recommendation["track_id"]]
+print(tracks_info)
+
+user_vector
+
+# Gotta Embed The SONG    
+#<iframe src="https://open.spotify.com/embed/track/{{ track_id }}" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+
+
+#if __name__ == '__main__':
+#    HOST = environ.get('SERVER_HOST', 'localhost')
+#   try:
+#       PORT = int(environ.get('SERVER_PORT', '5555'))
+#    except ValueError:
+#        PORT = 5555
+#    app.run(HOST, PORT)
+
